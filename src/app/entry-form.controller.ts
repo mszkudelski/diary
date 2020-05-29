c129cd7dd203118f28b2a23b88210ae9f3f89e8a
Marek Szkudelski <marek.szkudelski@sonalake.com>
2020-05-29 16:22:38 +0200

    Error handling in form

--- src/app/entry-form.controller.ts
+++ src/app/entry-form.controller.ts
@@ -11,9 +11,12 @@ export class EntryFormController {
     this.container.innerHTML = `
       <form class="form" action="#">
             <textarea class="form__text" name="content" placeholder="What's in your head?" id="" cols="30" rows="10"></textarea>
-            <input class="form__title" type="text" name="title" placeholder="Title">
-            <button type="submit" class="form__button">Save</button>
-            <button type="reset" class="form__button">Cancel</button>
+            <p class="form__error" id="error"></p>
+            <div>
+              <input class="form__title" type="text" name="title" placeholder="Title">
+              <button type="submit" class="form__button">Save</button>
+              <button type="reset" class="form__button">Cancel</button>
+            </div>
       </form>
     `;
 
@@ -25,19 +28,31 @@ export class EntryFormController {
       '[name="title"]',
     );
 
+    const errorElement = this.container.querySelector<HTMLElement>('#error');
+
     this.container
       .querySelector('button[type="submit"]')
       .addEventListener('click', (event: Event) => {
         event.preventDefault();
-        this.newEntryListeners.forEach((listener) =>
-          listener(
-            new DiaryEntry({
-              title: titleInput.value,
-              content: contentInput.value,
-              date: new Date(),
-            }),
-          ),
-        );
+        const executionSuccess = this.newEntryListeners.map((listener) => {
+          try {
+            listener(
+              new DiaryEntry({
+                title: titleInput.value,
+                content: contentInput.value,
+                date: new Date(),
+              }),
+            );
+            return true;
+          } catch (error) {
+            if (error.message === 'DIARY_ALREADY_HAS_ENTRY_WITH_SUCH_TITLE') {
+              errorElement.innerHTML = 'Entry with such title already exists';
+            }
+          }
+        });
+        if (executionSuccess.every(Boolean)) {
+          errorElement.innerHTML = '';
+        }
       });
   }
 
